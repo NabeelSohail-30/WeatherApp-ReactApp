1f1f974fc73a66b133fb4465dbcfaa55603cadd1
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-01-22 11:47:39 +0500

    working

--- src/components/Current/index.jsx
+++ src/components/Current/index.jsx
@@ -1,39 +1,26 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import rateLimit from 'axios-rate-limit';
 
 
 function Current() {
+    let city = "Karachi"
     const [data, setData] = useState([])
 
     useEffect(() => {
 
         const getCurrentWeather = async () => {
-            const options = {
-                method: 'GET',
-                url: 'https://weatherbit-v1-mashape.p.rapidapi.com/current',
-                params: { lon: '38.5', lat: '-78.5' },
-                headers: {
-                    'X-RapidAPI-Key': 'f1c3708e10msh1b4ab8a9141c588p1345cejsndd176f0e412d',
-                    'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com'
-                }
-            };
-
-            const api = rateLimit(axios.create(), { maxRequests: 5, perMilliseconds: 1000 });
-
             try {
-                const response = await api.get(options);
-                setData(response.data);
+                const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?units=metric&appid=6a6cb112b4746fd1d963422db62a0782&q=${city}`)
+                console.log(response);
+                const data = await response.data.data;
+                setData(data);
             } catch (error) {
-                if (error.response.status === 429) {
-                    console.log("Too many requests, please try again later.");
-                }
+                console.log("Error getting Weather: ", error);
             }
         }
-
         getCurrentWeather();
 
-    }, [])
+    }, [city])
 
 
     return <div>
@@ -44,23 +31,27 @@ function Current() {
 
 
         <div>
-            {data.map((item) => (
-                <div key={item.ts}>
-                    <h2>{item.city_name}</h2>
-                    <p>{item.weather.description}</p>
-                    <p>{item.temp}°C</p>
-                    <p>{item.app_temp}°C</p>
-                    <p>{item.wind_spd} m/s</p>
-                    <p>{item.wind_cdir_full}</p>
-                    <p>{item.rh}%</p>
-                    <p>{item.pres} hPa</p>
-                    <p>{item.slp} hPa</p>
-                    <p>{item.dewpt}°C</p>
-                    <p>{item.clouds}%</p>
-                    <p>{item.vis} km</p>
-                    <p>{item.snow} mm</p>
-                    <p>{item.precip} mm</p>
-                    <p>{item.ob_time}</p>
+            {data?.map((data) => (
+                <div key={data.id}>
+                    <h2>{data.name}</h2>
+                    <h3>{data.main.temp}</h3>
+                    <h3>{data.main.feels_like}</h3>
+                    <h3>{data.main.temp_min}</h3>
+                    <h3>{data.main.temp_max}</h3>
+                    <h3>{data.main.pressure}</h3>
+                    <h3>{data.main.humidity}</h3>
+                    <h3>{data.weather[0].main}</h3>
+                    <h3>{data.weather[0].description}</h3>
+                    <h3>{data.weather[0].icon}</h3>
+                    <h3>{data.wind.speed}</h3>
+                    <h3>{data.wind.deg}</h3>
+                    <h3>{data.sys.country}</h3>
+                    <h3>{data.sys.sunrise}</h3>
+                    <h3>{data.sys.sunset}</h3>
+                    <h3>{data.timezone}</h3>
+                    <h3>{data.dt}</h3>
+                    <h3>{data.id}</h3>
+                    <h3>{data.cod}</h3>
                 </div>
             ))}
         </div>
